0b196a38bdd6539703df78d71348b241e31cfe5f
taaage <61243007+taaage@users.noreply.github.com>
2021-12-06 22:01:15 +0100

    Add files via upload

---
+++ 1_string_concatenation.py
@@ -0,0 +1,15 @@
+# string concatenation using mad libs 
+
+#####################################
+
+color1 = input("Color 1: ")
+color2 = input("Color 2: ")
+food = input("Food: ")
+adj = input("Adjevtive: ")
+verb1 = input("Verb 1: ")
+verb2 = input("Verb2 2: ")
+
+madlib = f"Roses are {color1}, violets are {color2} and my favorite kind of food is {food}. \
+Computer programs are {adj} and assist people with {verb1} and help animals with {verb2}!"
+
+print (madlib)
