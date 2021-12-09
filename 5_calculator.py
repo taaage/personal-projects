7dba9b23a0dad9053e228eb7ab77f955b25f2e6b
oliverclind <78987636+oliverclind@users.noreply.github.com>
2021-12-09 01:40:37 +0100

    Update 5_calculator.py

--- 5_calculator.py
+++ 5_calculator.py
@@ -1,22 +1,38 @@
 # Calculator with two inputs
-
+# Written by: Tigge y un poco Oliver
 ############################
 
-print("Enter a number, operator (+, -, /, *) and another number")
+while True:
+    print("Enter a number, Enter an operator (+, -, /, *) and Enter another number")
+    
+    input1 = input("First number: ")
+    math_function = input("Math input: ")
+    input2 = input("Second number: ")
 
-input1 = input("First number: ")
-math_function = input("Math input: ")
-input2 = input("Second number: ")
+    if math_function == '+':
+        result = float(input1) + float(input2)
+        print(input1, "+", input2, "=", result)
+    elif math_function == '-':
+        result = float(input1) - float(input2)
+        print(input1, "-", input2, "=", result)
+    elif math_function == '/':
+        result = float(input1) / float(input2)
+        print(input1, "/", input2, "=", result)
+    elif math_function == '*':
+        result = float(input1) * float(input2)
+        print(input1, "*", input2, "=", result)
+    else:
+        result = "Not a valid operator"
 
-if math_function == '+':
-    result = int(input1) + int(input2)
-elif math_function == '-':
-    result = int(input1) - int(input2)
-elif math_function == '/':
-    result = int(input1) / int(input2)
-elif math_function == '*':
-    result = int(input1) * int(input2)
-else:
-    result = "Not a valid operator"
+    # check if user wants another calculation
+    # break the while loop if answer is no
 
-print (result)
+    next_calculation = input("Let's do another calculation? (yes/no): ")
+
+    if next_calculation == "yes":
+        print("New calculation")
+
+    elif next_calculation == "no":
+          break
+    else:
+        print("Invalid choice")
