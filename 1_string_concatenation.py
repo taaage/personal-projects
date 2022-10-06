b4682a0a7189c6f1cdc0698ce9c618b1b98da743
Tigge <tigge.nilsson@gmail.com>
2022-10-06 23:01:36 +0200

    changed small pieces

--- 1_string_concatenation.py
+++ 1_string_concatenation.py
@@ -3,18 +3,21 @@
 #####################################
 
 
-class madlib:
+class main:
 
     def __init__ (self, color1, color2, food):
         self.color1 = color1
         self.color2 = color2
         self.food = food
 
-madlib_1 = madlib('red', 'blue', 'burger')
-madlub_2 = madlib('green', 'yellow', 'pasta')
+madlib_1 = main('red', 'blue', 'burger')
+madlub_2 = main('green', 'yellow', 'pasta')
 
 
-print("Roses are " + madlib_1.color1 + ", violets are " + madlib_1.color2 + ", I love " + madlib_1.food)
-print("Roses are " + madlub_2.color1 + ", violets are " + madlub_2.color2 + ", I love " + madlub_2.food)
+one = ("Roses are " + madlib_1.color1 + ", violets are " + madlib_1.color2 + ", I love " + madlib_1.food)
+two = ("Roses are " + madlub_2.color1 + ", violets are " + madlub_2.color2 + ", I love " + madlub_2.food)
+
+print (one)
+print (two)
 
 #comment
