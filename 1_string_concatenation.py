f0ee1ef9b65ce00074634f3b334de2f8d479b07a
Tigge <tigge.nilsson@gmail.com>
2022-10-06 21:55:42 +0200

    created function instead

--- 1_string_concatenation.py
+++ 1_string_concatenation.py
@@ -2,14 +2,11 @@
 
 #####################################
 
-color1 = input("Color 1: ")
-color2 = input("Color 2: ")
-food = input("Food: ")
-adj = input("Adjevtive: ")
-verb1 = input("Verb 1: ")
-verb2 = input("Verb2 2: ")
+red = 'red'
+blue = 'blue'
+burger = 'burger'
 
-madlib = f"Roses are {color1}, violets are {color2} and my favorite kind of food is {food}. \
-Computer programs are {adj} and assist people with {verb1} and help animals with {verb2}!"
+def madlib (color1, color2, food):
+    print("Roses are " + color1 + ", violets are " + color2 + ", I love " + food)
 
-print (madlib)
+madlib(red, blue, burger)
